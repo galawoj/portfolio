fa9677cc4b3b65ff9e2b4a63994901c320817e13
unknown <galawoj@gmail.com>
2024-01-29 21:31:19 +0100

    ver2

--- assets/js/js.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('scroll',()=>{
-    
-    window.innerWidth>980 ? header.style.backgroundPosition=`left ${-0.04*window.scrollY}px` : header.style.backgroundPosition=null
-})
-
-console.log(window.innerWidth)
-
